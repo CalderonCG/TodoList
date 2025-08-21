fc116a7469f9bad445d7193b098899408aacf350
CalderonCodes <cgcalderondev@gmail.com>
2025-08-21 11:56:04 -0600

    :feat: add form submit

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,19 +1,61 @@
-import './Form.scss'
+import { useForm, type SubmitHandler } from "react-hook-form";
+import "./Form.scss";
+import { IoMdSend } from "react-icons/io";
 
+type FormFields = {
+  task: string;
+  deadline: string;
+  priority: "High" | "Medium" | "Low";
+};
 
 function Form() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormFields>();
+
+  const onSubmit: SubmitHandler<FormFields> = (data) => {
+    console.log(data);
+  };
+
   return (
-    <div className='form'>
-        <input className='form_task' placeholder='Write your task here' type="text" />
-        <input type="date" className='form_deadline' />
-        <select className='form_select' name="priority" id="priority">
-            <option value="">Priority</option>
-            <option value="High">High</option>
-            <option value="Medium">Medium</option>
-            <option value="Low">Low</option>
+    <div className="form_container">
+      <form className="form" onSubmit={handleSubmit(onSubmit)}>
+        <input
+          {...register("task", {
+            required: true,
+          })}
+          className="form_task"
+          placeholder="Write your task here"
+          type="text"
+        />
+        <input
+          {...register("deadline", { required: true })}
+          type="date"
+          className="form_deadline"
+        />
+        <select
+          {...register("priority", { required: true })}
+          className="form_select"
+          name="priority"
+          id="priority"
+        >
+          <option value="">Priority</option>
+          <option value="High">High</option>
+          <option value="Medium">Medium</option>
+          <option value="Low">Low</option>
         </select>
+        <button ><IoMdSend className="submit"/></button>
+      </form>
+
+      {errors.deadline || errors.priority || errors.task ? (
+        <p>Please complete all the fields</p>
+      ) : (
+        ""
+      )}
     </div>
-  )
+  );
 }
 
-export default Form
+export default Form;
