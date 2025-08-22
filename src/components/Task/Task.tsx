dbae7acc8a8aae4675d5be0bd62507313e90020e
CalderonCodes <cgcalderondev@gmail.com>
2025-08-22 15:45:21 -0600

    :fix: fix list height on smaller screens

--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -10,15 +10,25 @@ type TaskProps = {
 
 function Task({ task, handleToggle }: TaskProps) {
   return (
-    <div className={clsx('task', {checked_text : task.completed })}>
+    <div className={clsx("task", { checked_text: task.completed })}>
       {/* {`task_check ${task.completed? 'checked':''}`} onClick={()=>handleToggle({type:'toggle' , value:task.id})} */}
-      <button className={clsx('task_check', {checked: task.completed})}
-       onClick={()=>handleToggle({type:'toggle' , value:task.id})}>
-        {task.completed && <FaCheck/>}
+      <button
+        className={clsx("task_check", { checked: task.completed })}
+        onClick={() => handleToggle({ type: "toggle", value: task.id })}
+      >
+        {task.completed && <FaCheck />}
       </button>
       <p className="task_task">{task.task}</p>
       <p className="task_deadline">{task.deadline}</p>
-      <p className="task_priority">{task.priority}</p>
+      <p
+        className={clsx("task_priority", {
+          high: task.priority === "High" && !task.completed,
+          medium: task.priority === "Medium" && !task.completed,
+          low: task.priority === "Low" && !task.completed,
+        })}
+      >
+        {task.priority}
+      </p>
     </div>
   );
 }
